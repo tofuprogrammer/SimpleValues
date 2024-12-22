d055221afaf703ef7334dc7e71c82704bdcf2b6b
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-22 15:59:28 +0000

    Completed fruits task and punctuated comments

--- SimpleValues/main.swift
+++ SimpleValues/main.swift
@@ -1,17 +1,26 @@
 var myVariable = 42
 myVariable = 50
 let myConstant = 42
-// Declared a variable and a constant
+// Declared a variable and a constant.
 
 let implicitInteger = 70
 let implicitDouble = 70.0
 let explicitDouble: Double = 70.0
-// Implicitly declared an integer and a double and explicitly declared a double
+// Implicitly declared an integer and a double and explicitly declared a double.
 
 let explicitFloat: Float = 4
-// Completed the float declaration experiment
+// Completed the float declaration experiment.
 
 let label = "The width is "
 let width = 94
 let widthLabel = label + String(width)
 // Removing the String conversion gives the error "Binary operator '+' cannot be applied to operands of type 'String' and 'Int'.
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let orangeSummary = "I have \(oranges) oranges."
+let fruitSummary = "Therefore I have \(apples + oranges) fruits."
+/* Initialises two constants, a number of apples and a number of oranges.
+ Uses escape sequences to display the numbers contained in these constants in the output.
+ Uses escape sequences to perform integer maths before outputting the result to the console. */
